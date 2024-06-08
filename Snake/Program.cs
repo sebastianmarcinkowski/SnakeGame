05d4caf8d223c8f11a1b94f5bd06f335f70dc6c8
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2024-06-08 21:53:58 +0200

    Add board to game

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿var horizontal = '═';
+var vertical = '║';
+var topLeft = '╔';
+var topRight = '╗';
+var bottomLeft = '╚';
+var bottomRight = '╝';
+
+var width = 32;
+var height = 16;
+
+DrawBoard();
+
+void DrawBoard()
+{
+    Console.Clear();
+    Console.WriteLine($"{topLeft}{new string(horizontal, width - 2)}{topRight}");
+
+    for (int i = 0; i < height - 2; i++)
+    {
+        Console.WriteLine($"{vertical}{new string(' ', width - 2)}{vertical}");
+    }
+
+    Console.WriteLine($"{bottomLeft}{new string(horizontal, width - 2)}{bottomRight}");
+}
